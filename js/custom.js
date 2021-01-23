b4eb4bf281140c6a8992a71b9b60694b49fda00a
adamnikk <58403484+adamnik101@users.noreply.github.com>
2021-01-23 16:53:39 +0100

    s

--- js/custom.js
+++ js/custom.js
@@ -27,7 +27,7 @@ jQuery(document).ready(function($)
 			var data = JSON.stringify(result);
 		console.log(data);
 		for(let e in result){
-			console.log(e)
+			console.log(e.name)
 		}
 		},
 		error: function(xhr,status, error) { console.log(error); }
--- js/dist/custom.dev.js
+++ js/dist/custom.dev.js
@@ -29,7 +29,7 @@ jQuery(document).ready(function ($) {
       console.log(data);
 
       for (var e in result) {
-        console.log(e);
+        console.log(e.name);
       }
     },
     error: function error(xhr, status, _error) {
