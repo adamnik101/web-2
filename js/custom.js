2561d75158070f2ef1cdffcaeb6a900962f477c0
adamnikk <58403484+adamnik101@users.noreply.github.com>
2021-01-23 23:35:28 +0100

    s

--- js/custom.js
+++ js/custom.js
@@ -547,7 +547,7 @@ function price(item, discount) {
 function showMore(sectionId, parent, content, currentItem, data){
 		let animation = " animate"
 			for(let item in data.allGames){
-				console.log(data.allGames[item]);
+				console.log(item);
 				if(item > currentItem && data.allGames[item].newRelease && data.allGames[item].id > currentItem  && !data.allGames[item].shownHotSales && parent == "new" ){
 					displayItems(data.allGames[item].image.cover, data.allGames[item].name, data.allGames[item].publisher, price(data.allGames[item], data.allGames[item].price.discount), animation, sectionId);	
 					
