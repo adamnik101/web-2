e698d60da0ff1c66f68e02438ee7751239467e42
adamnikk <58403484+adamnik101@users.noreply.github.com>
2021-01-26 01:32:09 +0100

    s

--- js/dist/custom.dev.js
+++ js/dist/custom.dev.js
@@ -337,6 +337,9 @@ jQuery(document).ready(function ($) {
       var i = 0;
       var delay = 0;
       $("#" + sectionId).html(content);
+      $("#" + sectionId + " .animate").animate({
+        height: "500px"
+      });
       $("#" + sectionId + " .animate").each(function () {
         $("#" + sectionId + " .animate").eq(i).delay(600 * delay++).animate({
           bottom: "0",
